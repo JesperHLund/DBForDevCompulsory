c7e2c91be4b41e05c7274283ff069cd2cd947a8d
Jesper <jesperlund9@hotmail.com>
2025-01-17 15:41:28 +0100

    Add files via upload

---
+++ Schema Migrations Assignment/Model/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schema_Migrations_Assignment2.Model
+{
+    public class Products
+    {
+        //Create products based on Initial database schema.sql
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; } 
+        public int CategoryID { get; set; }
+
+        public Products(int productID, string productName, decimal productPrice, int categoryID)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            ProductPrice = productPrice;
+            CategoryID = categoryID;
+        }
+    }
+}
